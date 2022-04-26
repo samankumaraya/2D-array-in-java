edc5737227011c100afb741041937065b0d39c6b
Saman Kumara <94196963+samankumaraya@users.noreply.github.com>
2022-04-26 17:59:33 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,30 @@
+package RightTrianglePattern;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int [][] arr=new int[3][3];
+        Scanner sc= new Scanner(System.in);
+
+
+        for(int i=0;i<3;i++){
+            for( int j=0;j<3;j++){
+                System.out.println("Enter Element");
+                arr[i][j]=sc.nextInt();
+                System.out.println();
+            }
+        }
+        System.out.println("Printing Elements...");
+
+        for(int i=0;i<3;i++)
+        {
+            System.out.println();
+            for(int j=0;j<3;j++)
+            {
+                System.out.print(arr[i][j]+"\t");
+            }
+        }
+    }
+}
